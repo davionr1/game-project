55ee0cd806107eb1287d6e82e9c07d5379e6c422
Davion Roachford <davionr1@icloud.com>
2022-11-01 03:14:12 -0700

    working game, add win counter

--- play.js
+++ play.js
@@ -1,75 +1,93 @@
-
-const XClass = 'X'
-const OClass = 'O'
-const tilesElements = document.querySelectorAll('[data-tile]')
-const grid = document.getElementById('grid')
-const winningMessage = document.getElementById('outcome')
-const winningMessageText = document.querySelector('data-win')
-let XTurn
-const AligntoWin= [
+//declare 
+const X = "x"
+const O = "o"
+const combinationsToWin= [
     [0,1,2],
     [3,4,5],
     [6,7,8],
     [0,3,6],
     [2,5,8],
     [0,4,8],
-    [2,4,6]
+    [2,4,6],
+    [1,4,7]
 ]
-start()
+const tiles = document.querySelectorAll('[tile]')
+const grid = document.getElementById('grid')
+const endMessage = document.getElementById('endMessage') 
+const winningMessage = document.querySelector('[winText]')
+var winCounter = {X: 0, O: 0}
+let Oturn
+
+    start()
 
 function start(){
-    XTurn = false
-    tilesElements.forEach(tile => {
-        tile.addEventListener('click', whenCLicked, {once: true})
+    Oturn = false
+    tiles.forEach(tile => {
+        tile.classList.remove(X)
+        tile.classList.remove(O)
+        tile.addEventListener('click', whenClicked, {once: true})
     })
-    setGridHoverCLass()
+    endMessage.classList.remove('show')
+    
 }
 
-tilesElements.forEach(tile => {
-    tile.addEventListener('click', whenClicked, { once: true })
-})
-
-function whenCLicked(e){
-    const tile = e.target
-    const currentClass = XTurn ? XClass : OClass
-    placeTurn(tile, currentClass)
-    if (checkWin(currentClass)){
-        endGame(false)
-    }
-    changeTurns()
-    setGridHoverCLass()
+//actions to take when a box is clicked
+function whenClicked(e){
+const tile = e.target
+const currentClass = Oturn ? O : X
+placeMove(tile, currentClass)
+ 
+if (outcome(currentClass)){
+    end(false)
+}
+else if(ifDraw()){
+end(true)
+}
+else{
+changeTurns()
+}
 }
 
-function endGame(draw){
-    if (draw){
-
-    }
-    else{
-        winningMessageText.innerText = `${XTurn ? "X" : "O}"} Wins`
-    }
-    winningMessage.classList.add('display')
+//place x or o down
+function placeMove(tile, currentClass){
+    tile.classList.add(currentClass)
 }
 
-function placeTurn(tile, currentClass){
- tile.classList.add(currentClass)
-}
+//change turns
 function changeTurns(){
-    XTurn = !XTurn
+    Oturn = !Oturn
 }
-function setGridHoverCLass(){
-    grid.classList.remove(OClass)
-    grid.classList.remove(XClass)
-    if(XTurn){
-        grid.classList.add(XClass)
+
+//go through combinations to find winner
+    function outcome(currentClass){
+        return combinationsToWin.some(combinations => {
+         return combinations.every(index => {
+                return tiles[index].classList.contains(currentClass)
+            })
+        })
+    }
+//if it ends in a draw
+function ifDraw(){
+    return [...tiles].every(tiles => {
+        return tiles.classList.contains(O) || tiles.classList.contains(X)
+    })
+}
+//end game
+function end(draw){
+    if (draw){
+       winningMessage.innerHTML = "DRAW"
     }
     else{
-        grid.classList.add(OClass)
-    }
+        winningMessage.innerHTML = `${Oturn ? "O" : "X"} IS THE WINNER`
+        
+     }
+     endMessage.classList.add('show')
 }
-function checkWin(currentClass){
-    return AligntoWin.some(combination => {
-        return combination.every(index => {
-            return tilesElements[index].classList.contains(currentClass)
-        }) 
-    })
+//reset
+const reset = document.getElementById('reset')
+reset.addEventListener('click', start)
+
+//scoreboard
+function updateWincounter(){
+    ++winCounter
 }
